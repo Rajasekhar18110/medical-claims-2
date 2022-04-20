f917f894733460deb1d0a0b2a1a0cad0753a5891
Rajasekhar18110 <rajasekhar9959796507@gmail.com>
2022-04-20 14:24:36 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -55,9 +55,9 @@ function App() {
             <AuthProvider>
                 <Router>
                     <Routes>
-                        <Route path="/" element={<Login />} />
+                        <Route path="medical-claims-2/" element={<Login />} />
                         <Route path="/Signup" element={<Signup />} />
-                            {//<Route path="*" element={<Errorpage />} />}
+                        {/* <Route path="*" element={<Errorpage />} /> */}
                         <Route
                             path="/Home"
                             element={
